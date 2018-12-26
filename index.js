0919129829082240d37b4cc67cade403ff4bafda
Anthony Rush <par5618@icloud.com>
2018-12-26 21:57:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ function happyHolidays(string){
   return "Happy holidays!"
 }
 function happyHolidaysTo(string){
-  if (string === Christina) return `Happy holidays, ${Christina}!`}
+  if (Christina) return `Happy holidays, ${Christina}!`}
